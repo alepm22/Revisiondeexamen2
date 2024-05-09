5d6b4149a5bc9799b40b1703ad11f04b1ac3d115
alejandra <alejandra.palz@ucb.edu.bo>
2024-05-09 05:09:07 -0400

    no encuentra proyectos cuando no se tiene ninguno en la lista de proyectos

--- src/proyectos.spec.js
+++ src/proyectos.spec.js
@@ -1,20 +1,32 @@
-import buscarProyectoPor from "./proyectos.js";
+describe("Buscar", () => {
+  it("no encuentra proyectos cuando no se tiene ninguno en la lista de proyectos", () => { 
+      let proyectos = [];
+      expect(buscarProyecto("ejerciciol", proyectos)).toEqual("");
+  });
 
-describe("buscar proyecto por nombre", () => {
-  it("Deberia retornar el mensaje no tiene proyectos si se trata de realizar una busqueda sobre una lista de proyectos vacia", () => {
-    let proyectos=[]
-    expect(buscarProyectoPor("proyecto1",proyectos)).toEqual("No existen proyectos");
-  });
-  it("Deberia retornar el nombre del proyectos buscado si es que solo tenemos un proyecto", () => {
-    let proyectos=["proyecto1"]
-    expect(buscarProyectoPor("proyecto1",proyectos)).toEqual("proyecto1");
-  });
-  ("Deberia retornar el nombre del proyectos buscado si es que tenemos mas de un proyecto en proyectos", () => {
-    let proyectos=["proyecto1","proyecto2"]
-    expect(buscarProyectoPor("proyecto2",proyectos)).toEqual("proyecto2");
-  });
-  it("Deberia retornar los nombres de los proyectos que coincidan con la busqueda", () => {
-    let proyectos=["proyecto1","proyecto2","proyecto2"]
-    expect(buscarProyectoPor("proyecto2",proyectos)).toEqual("proyecto2,proyecto2");
-  });
+
 });
+
+function buscarProyecto(nombre, proyectos) {
+  if (proyectos.length === 0) {
+      return "";
+  }
+
+  let resultados = [];
+
+  for (const proyecto of proyectos) {
+      if (proyecto === nombre) {
+          resultados.push(proyecto);
+      } else if (proyecto.startsWith(nombre)) {
+          resultados.push(proyecto);
+      }
+  }
+
+  if (resultados.length === 0) {
+      return "";
+  } else if (resultados.length === 1) {
+      return resultados[0];
+  } else {
+      return resultados;
+  }
+}
