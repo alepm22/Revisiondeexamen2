83a148c49f1b029572cd11114c7e26482330bbc6
alejandra <alejandra.palz@ucb.edu.bo>
2024-05-08 20:45:14 -0400

    caso prueba donde Deberia retornar el nombre del proyectos buscado si es que solo tenemos un proyecto

--- src/presenter.js
+++ src/presenter.js
@@ -1,30 +1,15 @@
-import sumar from "./sumador";
-import multiplicar from "./multiplicacion";
+import sumar from "./proyectos";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#operacion-form");
+const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener('submit', function(event) {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
-  const submitButtonValue = event.submitter.value;
 
-  let resultado;
-
-  switch (submitButtonValue) {
-    case 'sumar':
-      resultado = sumar(firstNumber, secondNumber);
-      break;
-    case 'multiplicar':
-      resultado = multiplicar(firstNumber, secondNumber);
-      break;
-    default:
-      resultado = 'Operación no válida';
-  }
-
-  div.innerHTML = "<p>" + resultado + "</p>";
+  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
 });
---
+++ src/proyectos.js
@@ -0,0 +1,24 @@
+
+function buscarProyectoPor(nombre, proyectos){
+  const STRINGVACIO=""
+  const TAMANIOPROYECTOS=proyectos.length
+  let iteradorProyectos
+  let proyectosBuscados=[]
+  if(TAMANIOPROYECTOS==0)
+    {
+      return "No existen proyectos"
+    }
+  for (iteradorProyectos=0;iteradorProyectos<TAMANIOPROYECTOS;iteradorProyectos++){
+    let proyectoBuscado=proyectos[iteradorProyectos]
+    if (proyectoBuscado.startsWith(nombre))
+      {
+        proyectosBuscados.push(proyectos[iteradorProyectos])
+      }
+  }
+  if (proyectosBuscados.length==0)
+    {
+      return STRINGVACIO
+    }
+  return proyectosBuscados.join(",")
+}
+export default buscarProyectoPor;
