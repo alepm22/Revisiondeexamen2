b4646f19d56aed6b46be78a9cb5ea733f5bdda19
alejandra <alejandra.palz@ucb.edu.bo>
2024-05-09 05:06:17 -0400

    Deberia retornar los nombres de los proyectos que coincidan con la busqueda

--- src/proyectos.spec.js
+++ src/proyectos.spec.js
@@ -3,14 +3,18 @@ import buscarProyectoPor from "./proyectos.js";
 describe("buscar proyecto por nombre", () => {
   it("Deberia retornar el mensaje no tiene proyectos si se trata de realizar una busqueda sobre una lista de proyectos vacia", () => {
     let proyectos=[]
-    expect(buscarProyectoPor("proy1",proyectos)).toEqual("No existen proyectos");
+    expect(buscarProyectoPor("proyecto1",proyectos)).toEqual("No existen proyectos");
   });
   it("Deberia retornar el nombre del proyectos buscado si es que solo tenemos un proyecto", () => {
-    let proyectos=["proy1"]
-    expect(buscarProyectoPor("proy1",proyectos)).toEqual("proy1");
+    let proyectos=["proyecto1"]
+    expect(buscarProyectoPor("proyecto1",proyectos)).toEqual("proyecto1");
   });
   ("Deberia retornar el nombre del proyectos buscado si es que tenemos mas de un proyecto en proyectos", () => {
-    let proyectos=["proy1","proy2"]
-    expect(buscarProyectoPor("proy2",proyectos)).toEqual("proy2");
+    let proyectos=["proyecto1","proyecto2"]
+    expect(buscarProyectoPor("proyecto2",proyectos)).toEqual("proyecto2");
+  });
+  it("Deberia retornar los nombres de los proyectos que coincidan con la busqueda", () => {
+    let proyectos=["proyecto1","proyecto2","proyecto2"]
+    expect(buscarProyectoPor("proyecto2",proyectos)).toEqual("proyecto2,proyecto2");
   });
 });
